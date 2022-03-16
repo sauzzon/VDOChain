4dc6bdbd589493b399586b127ecd64c6720b0656
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-03-16 18:46:41 +0545

    Initial project structure

---
+++ blockchain/scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+
+  const Token = await hre.ethers.getContractFactory("VDOChain");
+  const vdochain = await Token.deploy();
+  await vdochain.deployed();
+
+  console.log("VDOChain address: ", vdochain.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
