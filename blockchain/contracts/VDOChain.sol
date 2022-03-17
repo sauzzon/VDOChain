1eea4b3ee5311c2668be380f3233fbde07415c2b
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-03-17 17:10:03 +0545

    First smart contract created

--- blockchain/contracts/VDOChain.sol
+++ blockchain/contracts/VDOChain.sol
@@ -2,4 +2,71 @@
 
 pragma solidity ^0.8.0;
 
-contract VDOChain {}
+contract VDOChain {
+    struct Video {
+        uint256 id;
+        address uploader;
+        string videoHash;
+        string thumbnailHash;
+        string detailsHash;
+        uint256 likesCount;
+        uint256 tipsCount;
+        uint256 commentsCount;
+    }
+
+    struct Comment {
+        address commenter;
+        string comment;
+    }
+
+    uint256 videoID = 0;
+    mapping(uint256 => Video) public videos;
+    mapping(uint256 => mapping(address => bool)) likes;
+    mapping(uint256 => Comment[]) comments;
+
+    function uploadVideo(
+        string memory _videoHash,
+        string memory _thumbnailHash,
+        string memory _detailsHash
+    ) public {
+        videoID++;
+        videos[videoID] = Video(
+            videoID,
+            msg.sender,
+            _videoHash,
+            _thumbnailHash,
+            _detailsHash,
+            0,
+            0,
+            0
+        );
+    }
+
+    function likeVideo(uint256 _id) public {
+        require(!likes[_id][msg.sender], "Video already liked");
+        likes[_id][msg.sender] = true;
+        videos[_id].likesCount++;
+    }
+
+    function isVideoLikedByAddress(uint256 _id) public view returns (bool) {
+        return likes[_id][msg.sender];
+    }
+
+    function tipVideo(uint256 _id) public {
+        require(msg.sender != videos[_id].uploader, "Can't tip yourself");
+        videos[_id].tipsCount++;
+    }
+
+    function commentVideo(uint256 _id, string memory _comment) public {
+        comments[_id].push(Comment(msg.sender, _comment));
+        videos[_id].commentsCount++;
+    }
+
+    function getVideoComments(uint256 _id)
+        public
+        view
+        returns (Comment[] memory)
+    {
+        return comments[_id];
+    }
+}
