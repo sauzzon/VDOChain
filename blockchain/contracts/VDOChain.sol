451205f405441c6e3b59d8b508597f180627d356
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-03-17 18:13:37 +0545

    modifier for valid uploaded videos

--- blockchain/contracts/VDOChain.sol
+++ blockchain/contracts/VDOChain.sol
@@ -12,6 +12,7 @@ contract VDOChain {
         uint256 likesCount;
         uint256 tipsCount;
         uint256 commentsCount;
+        bool isUploaded;
     }
 
     struct Comment {
@@ -19,30 +20,36 @@ contract VDOChain {
         string comment;
     }
 
-    uint256 videoID = 0;
+    uint256 public videoCount = 0;
     mapping(uint256 => Video) public videos;
     mapping(uint256 => mapping(address => bool)) likes;
     mapping(uint256 => Comment[]) comments;
+
+    modifier validVideo(uint256 _id) {
+        require(videos[_id].isUploaded == true, "Video not found");
+        _;
+    }
 
     function uploadVideo(
         string memory _videoHash,
         string memory _thumbnailHash,
         string memory _detailsHash
     ) public {
-        videoID++;
-        videos[videoID] = Video(
-            videoID,
+        videoCount++;
+        videos[videoCount] = Video(
+            videoCount,
             msg.sender,
             _videoHash,
             _thumbnailHash,
             _detailsHash,
             0,
             0,
-            0
+            0,
+            true
         );
     }
 
-    function likeVideo(uint256 _id) public {
+    function likeVideo(uint256 _id) public validVideo(_id) {
         require(!likes[_id][msg.sender], "Video already liked");
         likes[_id][msg.sender] = true;
         videos[_id].likesCount++;
@@ -52,12 +59,15 @@ contract VDOChain {
         return likes[_id][msg.sender];
     }
 
-    function tipVideo(uint256 _id) public {
+    function tipVideo(uint256 _id) public validVideo(_id) {
         require(msg.sender != videos[_id].uploader, "Can't tip yourself");
         videos[_id].tipsCount++;
     }
 
-    function commentVideo(uint256 _id, string memory _comment) public {
+    function commentVideo(uint256 _id, string memory _comment)
+        public
+        validVideo(_id)
+    {
         comments[_id].push(Comment(msg.sender, _comment));
         videos[_id].commentsCount++;
     }
